a264876695c288f1b7f5f5356e36b7c1262ec846
Md. Ashik Hassan Bhuiyan <sjb.25003@gmail.com>
2025-02-02 11:16:01 +0600

    project setup completed

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bulipe Tech Practical Test
+
+To clone the project in your device:
+`git clone `
+To Run the Project in Development mode:
+`npm run dev`
+
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      
+      <h1 className='text-5xl font-bold text-green-500'>Vite + React</h1>
+      <button className="btn btn-accent">Accent</button>
+      
+    </div>
+  )
+}
+
+export default App
